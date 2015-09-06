5bb44e38ff9fc714c7ab15efdff06c6c3902cf6f
MabezDev <mabez123@gmail.com>
2015-09-06 01:38:10 +0100

    Can communicate with smartwatch and give it the current date

--- src/com/mabezdev/MabezWatch/Main.java
+++ src/com/mabezdev/MabezWatch/Main.java
@@ -1,27 +1,35 @@
 package com.mabezdev.MabezWatch;
 
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Set;
+import java.util.UUID;
+
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 
 public class Main extends Activity {
 
     private static final int REQUEST_ENABLE_BT = 1;
     private Button onBtn;
     private Button offBtn;
+
+    BluetoothSocket socket;
     private Button listBtn;
     private Button findBtn;
     private TextView text;
@@ -29,6 +37,7 @@ public class Main extends Activity {
     private Set<BluetoothDevice> pairedDevices;
     private ListView myListView;
     private ArrayAdapter<String> BTArrayAdapter;
+    private OutputStream outputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +86,18 @@ public class Main extends Activity {
             myListView.setAdapter(BTArrayAdapter);
 
             // setup onclick listener for items in list view so that we connect from there we want to run bg program feeding the smart watch info
+            myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    for(BluetoothDevice d:pairedDevices){
+                        if(d.getAddress().equals(BTArrayAdapter.getItem(position).split("\n")[1])){
+                            System.out.println("Found BT Device trying to connect");
+                            connect(d);
+                        }
+                    }
+
+                }
+            });
         }
     }
 
@@ -162,7 +183,55 @@ public class Main extends Activity {
     protected void onDestroy() {
         // TODO Auto-generated method stub
         super.onDestroy();
-        unregisterReceiver(bReceiver);
+        if(bReceiver==null) {
+            unregisterReceiver(bReceiver);
+        }
+    }
+
+
+    protected void connect(BluetoothDevice device) {
+        //BluetoothSocket socket = null;
+        try {
+            if(socket!=null){
+                socket=null;
+            }
+            //Create a Socket connection: need the server's UUID number of registered
+            socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"));
+
+            socket.connect();
+            Log.d("EF-BTBee", ">>Client connectted");
+
+
+            outputStream = socket.getOutputStream();
+            outputStream.write(DateFormat.getDateTimeInstance().format(new Date()).getBytes()); //replace with func to send all necessary data ie weather etc
+
+        } catch (IOException e) {
+            Log.e("EF-BTBee", "", e);
+        } finally {
+            disconnect();
+        }
+    }
+
+    private void disconnect(){
+        if (socket != null) {
+            try {
+                Log.d("EF-BTBee", ">>Client Close");
+                // give a chance to send final message
+                try {
+                    Thread.sleep(1000);
+                }catch (Exception e){
+
+                }
+                //close streams
+                outputStream.close();
+                socket.close();
+                socket=null;
+                finish();
+                return;
+            } catch (IOException e) {
+                Log.e("EF-BTBee", "", e);
+            }
+        }
     }
 
 }
